278426619e44b033497d70ee23c12300ff81c7e7
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2024-04-09 18:51:19 -0300

    Add cn util

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 import { prisma } from '@/lib/prisma';
+import { cn } from '@/lib/cn';
+import { cva, VariantProps } from 'class-variance-authority';
 
-export { prisma };
+export { prisma, cn, cva, type VariantProps };
