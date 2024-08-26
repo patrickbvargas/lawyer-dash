b367ad4f554d2946c17a66e594d0458a102e6190
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2024-08-26 20:57:45 +0000

    refactor: remove unused ref for Card component
    - Removed unused `forwardRef` from all components of Card.

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,81 +1,60 @@
 import * as React from 'react';
-import { ENUM } from '@/constants/enum';
 import { cn, cva, type VariantProps } from '@/utils';
 
 interface RootProps extends React.HTMLAttributes<HTMLDivElement> {}
-export const Root = React.forwardRef<HTMLDivElement, RootProps>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn(
-        'flex cursor-pointer flex-col gap-2 rounded-lg border-l-4 border-transparent py-3 pl-3 pr-4 transition duration-300 hover:border-accent',
-        'bg-white shadow-sm',
-        'dark:bg-neutral-800 dark:shadow-none',
-        className,
-      )}
-      {...props}
-    />
-  ),
+export const Root = ({ className, ...props }: RootProps) => (
+  <div
+    className={cn(
+      'flex cursor-pointer flex-col gap-2 rounded-lg border-l-4 border-transparent py-3 pl-3 pr-4 transition duration-300 hover:border-accent',
+      'bg-white shadow-sm',
+      'dark:bg-neutral-800 dark:shadow-none',
+      className,
+    )}
+    {...props}
+  />
 );
-Root.displayName = 'Card.Root';
 
 interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
-export const Header = React.forwardRef<HTMLDivElement, HeaderProps>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn('flex items-center justify-between', className)}
-      {...props}
-    />
-  ),
+export const Header = ({ className, ...props }: HeaderProps) => (
+  <div
+    className={cn('flex items-center justify-between', className)}
+    {...props}
+  />
 );
-Header.displayName = 'Card.Header';
 
 interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {}
-export const Title = React.forwardRef<HTMLHeadingElement, TitleProps>(
-  ({ className, ...props }, ref) => (
-    <h2
-      ref={ref}
-      className={cn(
-        'truncate text-base font-medium',
-        'text-gray-700',
-        'dark:text-neutral-200',
-        className,
-      )}
-      {...props}
-    />
-  ),
+export const Title = ({ className, ...props }: TitleProps) => (
+  <h2
+    className={cn(
+      'truncate text-base font-medium',
+      'text-gray-700',
+      'dark:text-neutral-200',
+      className,
+    )}
+    {...props}
+  />
 );
-Title.displayName = 'Card.Title';
 
 interface ContentProps extends React.HTMLAttributes<HTMLDivElement> {}
-export const Content = React.forwardRef<HTMLDivElement, ContentProps>(
-  ({ className, ...props }, ref) => (
-    <div
-      ref={ref}
-      className={cn('grid grid-cols-1 gap-3.5 px-2 sm:grid-cols-2', className)}
-      {...props}
-    />
-  ),
+export const Content = ({ className, ...props }: ContentProps) => (
+  <div
+    className={cn('grid grid-cols-1 gap-3.5 px-2 sm:grid-cols-2', className)}
+    {...props}
+  />
 );
-Content.displayName = 'Card.Content';
 
 interface DividerProps extends React.HTMLAttributes<HTMLHRElement> {}
-export const Divider = React.forwardRef<HTMLHRElement, DividerProps>(
-  ({ className, ...props }, ref) => (
-    <hr
-      ref={ref}
-      className={cn(
-        'rounded-full border-t-2',
-        'border-gray-100',
-        'dark:border-neutral-700',
-        className,
-      )}
-      {...props}
-    />
-  ),
+export const Divider = ({ className, ...props }: DividerProps) => (
+  <hr
+    className={cn(
+      'rounded-full border-t-2',
+      'border-gray-100',
+      'dark:border-neutral-700',
+      className,
+    )}
+    {...props}
+  />
 );
-Divider.displayName = 'Card.Divider';
 
 const fieldVariants = cva('truncate text-sm', {
   variants: {
@@ -94,24 +73,26 @@ interface FieldProps
   label: string;
   value: string | number | undefined | null;
 }
-export const Field = React.forwardRef<HTMLDListElement, FieldProps>(
-  ({ variant, label, value, className, ...props }, ref) => (
-    <dl
-      ref={ref}
-      className={cn(
-        'flex flex-col gap-1',
-        'text-gray-500',
-        'dark:text-neutral-300',
-        className,
-      )}
-      {...props}
-    >
-      <dt className="truncate text-xs font-semibold uppercase">{label}</dt>
-      <dd className={fieldVariants({ variant })}>{value || ''}</dd>
-    </dl>
-  ),
+export const Field = ({
+  variant,
+  label,
+  value,
+  className,
+  ...props
+}: FieldProps) => (
+  <dl
+    className={cn(
+      'flex flex-col gap-1',
+      'text-gray-500',
+      'dark:text-neutral-300',
+      className,
+    )}
+    {...props}
+  >
+    <dt className="truncate text-xs font-semibold uppercase">{label}</dt>
+    <dd className={fieldVariants({ variant })}>{value || ''}</dd>
+  </dl>
 );
-Field.displayName = 'Card.Field';
 
 const badgeVariants = cva(
   'rounded-md px-2.5 py-1 text-xs font-semibold uppercase text-white place-self-end truncate',
@@ -131,15 +112,8 @@ interface BadgeProps
     VariantProps<typeof badgeVariants> {
   label: string;
 }
-export const Badge = React.forwardRef<HTMLSpanElement, BadgeProps>(
-  ({ variant, label, className, ...props }, ref) => (
-    <span
-      ref={ref}
-      className={cn(badgeVariants({ variant }), className)}
-      {...props}
-    >
-      {label}
-    </span>
-  ),
+export const Badge = ({ variant, label, className, ...props }: BadgeProps) => (
+  <span className={cn(badgeVariants({ variant }), className)} {...props}>
+    {label}
+  </span>
 );
-Badge.displayName = 'Card.Badge';
