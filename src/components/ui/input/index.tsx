da8d92dce99d378cf7fe677fbb611fd53a59cbdb
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2024-09-12 00:57:10 +0000

    style: Update Input component style
      - Apply default base style for form fields

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
-import { cn } from '@/utils';
+import { cn, type VariantProps } from '@/utils';
+import { formFieldVariants } from '@/styles';
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof formFieldVariants> {}
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type = 'text', ...props }, ref) => {
+  ({ variant, className, type = 'text', ...props }, ref) => {
     return (
       <input
         ref={ref}
         type={type}
         className={cn(
-          'transition duration-300 flex h-10 w-full rounded px-3.5 py-2 ring-1 text-sm font-normal focus:outline-none focus-visible:ring-2 disabled:opacity-50 disabled:cursor-not-allowed file:border-0 file:bg-transparent file:text-sm file:font-medium',
-          'bg-white text-gray-500 ring-gray-200 placeholder:text-gray-400 hover:ring-2 hover:ring-gray-200 focus:ring-gray-200 file:text-gray-500',
-          'dark:bg-neutral-800 dark:text-neutral-300 dark:ring-neutral-700/50 dark:placeholder:text-neutral-500 dark:hover:ring-0 dark:hover:bg-neutral-700 dark:focus:ring-neutral-600 dark:file:text-neutral-300',
+          formFieldVariants({ variant }),
+          'file:border-0 file:bg-transparent file:text-sm file:font-medium',
+          'file:text-gray-500',
+          'dark:file:text-neutral-300',
           className,
         )}
         {...props}
