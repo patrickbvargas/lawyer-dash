e83ce3be15fbe6acc5dccff9428bc44cb0a011a2
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2024-10-09 22:04:47 +0000

    refactor: Remove getPercentLocaleString Utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,8 @@
 import { cn } from './cn';
 import { formatter } from './formatter';
 import { cva, type VariantProps } from './cva';
-import { getPercentLocaleString } from './locale-string';
 import { getPaginationParamsFromQuery } from './pagination';
 
-export {
-  cn,
-  cva,
-  formatter,
-  getPaginationParamsFromQuery,
-  getPercentLocaleString,
-};
+export { cn, cva, formatter, getPaginationParamsFromQuery };
 
 export type { VariantProps };
--- src/utils/locale-string/index.ts
+++
@@ -1,5 +0,0 @@
-export function getPercentLocaleString(percent: number) {
-  return percent.toLocaleString('pt-BR', {
-    style: 'percent',
-  });
-}
