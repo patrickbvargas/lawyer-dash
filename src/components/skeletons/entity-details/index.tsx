5509a382396ad586c895ff1401102e6fa3f5083c
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2024-11-10 17:14:52 +0000

    style: Adjust Padding in EntityDetailsSkeleton

--- src/components/skeletons/entity-details/index.tsx
+++ src/components/skeletons/entity-details/index.tsx
@@ -28,7 +28,7 @@ const EntityDetailsGroupSkeleton = () => {
         <Skeleton className="h-6 w-3/4" />
         <Section.Content>
           {Array.from({ length: 4 }).map((_, index) => (
-            <div className="flex flex-col gap-3.5 pl-3.5 w-3/5" key={index}>
+            <div className="flex flex-col gap-3.5 w-3/5" key={index}>
               <DefinitionListSkeleton />
             </div>
           ))}
